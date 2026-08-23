5aef40a9b9077c908fd0be605cb70df4f4268df3
Abhimanyu Jakhar <abhimanyujakhar996@gmail.com>
2026-08-23 12:00:00 +0530

    Day 14 solutions

---
+++ day14/14.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0, odd = 1;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + odd;
+        odd = odd + 2;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
---
+++ day14/14.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int product = 1;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i = i + 2)
+    {
+        product = product * i;
+    }
+
+    printf("%d", product);
+
+    return 0;
+}
