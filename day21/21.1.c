01909ff19c1d9915261622b1086e84daa14a4f79
Abhimanyu Jakhar <abhimanyujakhar996@gmail.com>
2026-08-30 12:00:00 +0530

    Day 21 solutions

---
+++ day21/21.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, first, last, digits = 1, temp, result;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp >= 10)
+    {
+        temp = temp / 10;
+        digits = digits * 10;
+    }
+
+    first = n / digits;
+    last = n % 10;
+
+    result = n - first * digits - last;
+    result = result + last * digits + first;
+
+    printf("%d", result);
+
+    return 0;
+}
