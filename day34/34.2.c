65e3380df774c3a59432e8c8ee21ed96156af91f
Abhimanyu Jakhar <abhimanyujakhar996@gmail.com>
2026-09-12 12:00:00 +0530

    Day 34 solutions

---
+++ day34/34.2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, position;
+    int arr[100];
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &position);
+
+    for (i = position - 1; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
